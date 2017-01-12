c3f75bacf1c87fd63e7d58f9ba60b25eb8140d88
davreymar <davreymar@alum.us.es>
2017-01-12 19:02:59 +0100

    Añadidas dependencias

--- M01/src/main/java/es/egc/app/App.java
+++ M01/src/main/java/es/egc/app/App.java
@@ -1,5 +1,4 @@
 package es.egc.app;
-
 /**
  * Hello world!
  *
@@ -8,6 +7,8 @@ public class App
 {
     public static void main( String[] args )
     {
+	
+
         System.out.println( "Hello World!" );
-    }
+	    }
 }
